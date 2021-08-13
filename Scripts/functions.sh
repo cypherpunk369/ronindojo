62ada72231552725a30b6cc354b8969ad88342d3
likewhoa <likewhoa@weboperative.com>
2021-08-13 00:11:15 -0400

    Make sure we set random passwords for docker-mempool.conf and fix syntax in sed command while removing redundant use of sed

--- Scripts/functions.sh
+++ Scripts/functions.sh
@@ -1229,7 +1229,7 @@ _mempool_conf() {
     mempool_conf="conf"
     test -f "${dojo_path_my_dojo}"/conf/docker-mempool.conf || mempool_conf="conf.tpl"
 
-    if [ "${mempool_conf}" = "conf" ]; then # Existing install
+    if [ "${mempool_conf}" = "conf" ] && ! grep -q 'MYSQL_USER=mempool' "${dojo_path_my_dojo}"/conf/docker-mempool.conf; then # Existing install
         MEMPOOL_MYSQL_USER=$(grep MYSQL_USER "${dojo_path_my_dojo}"/conf/docker-mempool.conf | cut -d '=' -f2)
         MEMPOOL_MYSQL_PASSWORD=$(grep MYSQL_PASSWORD "${dojo_path_my_dojo}"/conf/docker-mempool.conf | cut -d '=' -f2)
         MEMPOOL_MYSQL_ROOT_PASSWORD=$(grep MYSQL_ROOT_PASSWORD "${dojo_path_my_dojo}"/conf/docker-mempool.conf | cut -d '=' -f2)
@@ -1247,7 +1247,7 @@ _mempool_conf() {
     sed -i -e 's/MEMPOOL_INSTALL=.*$/MEMPOOL_INSTALL=on/' \
     -e "s/MYSQL_USER=.*$/MYSQL_USER=${MEMPOOL_MYSQL_USER}/" \
     -e "s/MYSQL_PASSWORD=.*$/MYSQL_PASSWORD=${MEMPOOL_MYSQL_PASSWORD}/" \
-    -e "s/MYSQL_ROOT_PASSWORD=.$/MYSQL_ROOT_PASSWORD=${MEMPOOL_MYSQL_ROOT_PASSWORD}/" "${dojo_path_my_dojo}"/conf/docker-mempool."${mempool_conf}"
+    -e "s/MYSQL_ROOT_PASSWORD=.*$/MYSQL_ROOT_PASSWORD=${MEMPOOL_MYSQL_ROOT_PASSWORD}/" "${dojo_path_my_dojo}"/conf/docker-mempool."${mempool_conf}"
 }
 
 #
