63cfa4828254f29a06a0450f81ab4925412b400c
dammkewl <dammkewl>
2022-05-04 00:03:30 +0200

    The status check in _ufw_rule_add actually prevented a second rule for the same network being added, simplified the code before adding such checks again.

--- Scripts/network-check.sh
+++ Scripts/network-check.sh
@@ -5,15 +5,6 @@ interface_current=$(ip route get 1 | awk '{print $5}')
 network_current="$(ip route | grep $interface_current | grep -v default | awk '{print $1}')"
 ronin_data_dir=$1
 
-_ufw_rule_add(){
-    network=$1
-    port=$2
-
-    if ! sudo ufw status | grep "${network}" >/dev/null; then
-        sudo ufw allow from "$network" to any port "$port" >/dev/null
-    fi
-}
-
 _backup_network_info(){
     echo -e "ip=${ip_current}\nnetwork=${network_current}\n" >> "${ronin_data_dir}/ip.txt"
 }
@@ -32,8 +23,8 @@ elif sudo ufw status | head -n 1 | grep "Status: active" >/dev/null; then
     #while sudo ufw status | grep "${network}"; do
     #    sudo ufw status numbered | grep "${network}" | head -n 1 | sed -E 's/\[\s*([0-9]+)\].*/\1/' | xargs -n 1 sudo ufw --force delete
     #done
-    _ufw_rule_add "${network_current}" "80"
-    _ufw_rule_add "${network_current}" "22"
+    sudo ufw allow from "${network_current}" to any port "80" >/dev/null
+    sudo ufw allow from "${network_current}" to any port "22" >/dev/null
     sudo ufw reload
     _backup_network_info
 else 
