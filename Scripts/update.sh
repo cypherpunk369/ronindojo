dc2ef48598ab6b541053a2d636ff4f9f28b83168
Pavel Ševčík <pavel.sevcik@protonmail.com>
2022-06-02 12:04:19 +0200

    Rename update function so the update gets installed properly

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -449,7 +449,7 @@ _update_29() {
 }
 
 # Add service to auto detect network change, for keeping UFW ruleset up to date
-_update_30() {
+_update_31() {
 
     # Check if Network check is implemented. If not install and run it.
     if [ ! -f /etc/systemd/system/ronin.network.service ]; then
@@ -459,5 +459,5 @@ _update_30() {
     fi
 
     # Finalize
-    touch "$HOME"/.config/RoninDojo/data/updates/30-"$(date +%m-%d-%Y)"
+    touch "$HOME"/.config/RoninDojo/data/updates/31-"$(date +%m-%d-%Y)"
 }
