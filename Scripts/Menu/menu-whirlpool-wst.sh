bba8af18609259291ab7839d5d3fb8d60913fc1f
dammkewl <dammkewl>
2022-06-24 22:08:32 +0200

    Fixed print usage in menu-whirlpool-wst.sh

--- Scripts/Menu/menu-whirlpool-wst.sh
+++ Scripts/Menu/menu-whirlpool-wst.sh
@@ -5,13 +5,7 @@
 . "$HOME"/RoninDojo/Scripts/functions.sh
 
 if [ ! -d "$HOME"/Whirlpool-Stats-Tool ]; then
-    cat <<EOF
-${red}
-***
-Installing Whirlpool Stat Tool...
-***
-${nc}
-EOF
+    _print_message "Installing Whirlpool Stat Tool..."
     _sleep
 
     bash "$HOME"/RoninDojo/Scripts/Install/install-wst.sh
@@ -20,66 +14,21 @@ fi
 _sleep
 cd "$HOME"/Whirlpool-Stats-Tool/whirlpool_stats || exit
 
-cat <<EOF
-${red}
-Whirlpool Stat Tool INSTRUCTIONS:
-${nc}
-EOF
-
-_sleep
-# instructions are given to user
-
-cat <<EOF
-${red}
-Download in the working directory a snaphot for the 0.01BTC pools:
-${nc}
-download 001
-EOF
-
-_sleep
-
-cat <<EOF
-${red}
-Load and compute the statistcs for the snaphot:
-${nc}
-load 001
-EOF
-
-_sleep
-
-cat <<EOF
-${red}
-Display the metrics computed for a transaction stored in the active snapshot:
-${nc}
-score <ENTER TXID OF DESIRED 0.01 BTC transaction>
-EOF
-
-_sleep
-
-cat <<EOF
-${red}
-Sample output...
-${nc}
-Backward-looking metrics for the outputs of this mix:
-    anonset = 92
-    spread = 89%
-
-Forward-looking metrics for the outputs of Tx0s having this transaction as their first mix:
-    anonset = 127
-    spread = 76%
-EOF
-
-_sleep
-
-cat <<EOF
-${red}
-***
-Type: 'quit' at anytime to exit the Whirlpool Statitics Tool.
-***
-EOF
+_print_message "Whirlpool Stat Tool INSTRUCTIONS:"
+_print_message "Download in the working directory a snaphot for the 0.01BTC pools:" "download 001"
+_print_message "Load and compute the statistcs for the snaphot:" "load 001"
+_print_message "Display the metrics computed for a transaction stored in the active snapshot:" "score <ENTER TXID OF DESIRED 0.01 BTC transaction>"
+_print_message "Sample output..." \
+  "Backward-looking metrics for the outputs of this mix:" \
+  "    anonset = 92" \
+  "    spread = 89%" \
+  "" \
+  "Forward-looking metrics for the outputs of Tx0s having this transaction as their first mix:" \
+  "    anonset = 127" \
+  "    spread = 76%"
+_print_message "Type: 'quit' at anytime to exit the Whirlpool Statitics Tool."
 
 _pause continue
-# press any key to continue
 
 if ! pipenv run python wst.py -w=/tmp 2>/dev/null; then
     _check_pkg "pipenv" "python-pipenv"
@@ -95,13 +44,11 @@ EOF
 
     cd .. || exit
 
-    # Upgrade dependencies
     pipenv update &>/dev/null
 
     cd - &>/dev/null || exit
     pipenv run python wst.py -w=/tmp
 fi
-# run wst.py
 
 _pause return
 bash -c "${ronin_samourai_toolkit_menu}"
