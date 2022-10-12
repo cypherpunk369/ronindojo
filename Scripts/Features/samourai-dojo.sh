d7e28dc8d26ec144fdf1a547426e927c83bab4d3
dammkewl <dammkewl>
2022-10-13 00:16:30 +0200

    Implemented a first iteration of samourai-dojo.sh for just downloading and removing the repo.

--- Scripts/Features/samourai-dojo.sh
+++ Scripts/Features/samourai-dojo.sh
@@ -1,3 +1,52 @@
 #!/bin/bash
 # shellcheck source=Scripts/Features/samourai-dojo.sh
 
+###########
+# IMPORTS #
+###########
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+. $SCRIPTPATH/../Functions/packages.sh
+
+#############
+# CONSTANTS #
+#############
+
+samourai_repo='https://code.samourai.io/ronindojo/samourai-dojo.git'
+samourai_path="${HOME}/dojo"
+
+#############
+# FUNCTIONS #
+#############
+
+_download_dojo() {
+	cd "$HOME" || exit
+	git clone -q "${samourai_repo}" dojo 2>/dev/null
+	cd -
+
+	# cd "${samourai_path}" || exit
+	# git checkout -q -f "${samourai_commitish}"
+}
+
+_remove_dojo() {
+	rm -rf samourai_path
+}
+
+####################
+# SCRIPT EXECUTION #
+####################
+
+case $1 in
+    download)
+        _download_dojo
+    ;;
+    _remove_dojo)
+        _uninstall_wst
+    ;;
+    *)
+        echo "Unknown option '$1'"
+        exit 1
+    ;;
+esac
