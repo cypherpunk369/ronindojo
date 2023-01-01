2b01419747e1321732aca42090677b2e43881e57
BTCxZelko <btcxzelko@protonmail.com>
2023-01-01 22:19:09 +0000

    increase upper bound sequence in the for loop.

--- Scripts/functions.sh
+++ Scripts/functions.sh
@@ -104,7 +104,7 @@ _call_update_scripts() {
         for i in $(seq 1 9); do
             echo "skipped" > "$HOME"/.config/RoninDojo/data/updates/0${i}-"$(date +%m-%d-%Y)"
         done
-        for i in $(seq 10 35); do # make sure the upper bound of this for loop here, stays up-to-date with the update numbering
+        for i in $(seq 10 36); do # make sure the upper bound of this for loop here, stays up-to-date with the update numbering
             echo "skipped" > "$HOME"/.config/RoninDojo/data/updates/${i}-"$(date +%m-%d-%Y)"
         done
     fi
