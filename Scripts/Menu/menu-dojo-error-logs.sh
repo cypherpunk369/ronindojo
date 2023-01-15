01ae8adda1b890593d2f48c7de9b156a5d508b10
dammkewl <dammkewl>
2023-01-16 00:19:52 +0100

    Fixed occurrence of SC2249

--- Scripts/Menu/menu-dojo-error-logs.sh
+++ Scripts/Menu/menu-dojo-error-logs.sh
@@ -32,9 +32,7 @@ case $CHOICE in
             cd "$dojo_path_my_dojo" || exit
             _print_message "Press Ctrl + C to exit at any time..."
             _sleep
-            ;;
-esac
-case $CHOICE in
+            ;;&
         1)
             ./dojo.sh logs bitcoind -n 200 | grep -i 'error'
             ;;
