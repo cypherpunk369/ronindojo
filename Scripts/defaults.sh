dce70caf5249900420a6f080a660d7b851a46983
BTCxZelko <BTCxZelko@protonmail.com>
2023-03-25 21:03:08 -0700

    modify the boltzmann repo to the ronindojo branch that contains the np.bool_ fix

--- Scripts/defaults.sh
+++ Scripts/defaults.sh
@@ -116,7 +116,7 @@ ronin_dojo_branch="origin/release/v2.0.x" # defaults to origin/master
 ronin_dojo_repo="https://code.samourai.io/ronindojo/RoninDojo.git"
 samourai_repo='https://code.samourai.io/ronindojo/samourai-dojo.git'
 samourai_commitish="v1.15.1" # Tag release
-boltzmann_repo='https://code.samourai.io/oxt/boltzmann.git'
+boltzmann_repo='https://code.samourai.io/ronindojo/boltzmann.git'
 whirlpool_stats_repo='https://code.samourai.io/whirlpool/whirlpool_stats.git'
 ronin_ui_repo="https://code.samourai.io/ronindojo/ronin-ui.git"
 
