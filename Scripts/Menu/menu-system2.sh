bcbeb8ab5a612ed00f41327ba5247cbc73376400
Damm Kewl <25-dammkewl@users.noreply.code.samourai.io>
2023-03-29 22:28:52 +0200

    Removed dojo uninstall

--- Scripts/Menu/menu-system2.sh
+++ Scripts/Menu/menu-system2.sh
@@ -13,8 +13,7 @@ OPTIONS=(1 "Networking"
          3 "Change Root Password"
          4 "Lock Root User"
          5 "Unlock Root User"
-         6 "Uninstall RoninDojo"
-         7 "Go Back")
+         6 "Go Back")
 
 CHOICE=$(dialog --clear \
                 --title "$TITLE" \
@@ -50,76 +49,6 @@ case $CHOICE in
         sudo passwd -u root
         ;;
     6)
-        if [ ! -d "${dojo_path}" ]; then
-            _is_dojo "${ronin_system_menu2}"
-            exit
-        fi
-
-        _print_message "Uninstalling RoninDojo, press Ctrl+C to exit if needed!"
-        _sleep 10 --msg "Uninstalling in"
-
-        _stop_dojo
-
-        "${dojo_data_bitcoind_backup}" && _dojo_data_bitcoind_backup
-
-        "${dojo_data_indexer_backup}" && _dojo_data_indexer_backup
-
-        _print_message "Uninstalling RoninDojo..."
-
-        "${tor_backup}" && _tor_backup
-
-        sudo rm -rf "${install_dir}/docker" &>/dev/null
-
-        _is_mempool && _mempool_uninstall
-
-        _is_bisq && _bisq_uninstall
-
-        _uninstall_network_check_service
-
-        _print_message "Removing Samourai Dojo Server..."
-
-        cd "$dojo_path_my_dojo" || exit
-
-        if ./dojo.sh uninstall --auto; then
-            _is_ronin_ui && _ronin_ui_uninstall
-
-            _is_fan_control_installed && _fan_control_uninstall
-
-            if [ -d "${HOME}"/Whirlpool-Stats-Tool ]; then
-                cd "${HOME}"/Whirlpool-Stats-Tool || exit
-
-                _print_message "Uninstalling Whirlpool Stats Tool..."
-
-                pipenv --rm &>/dev/null
-                cd - 1>/dev/null || exit
-                rm -rf "${HOME}"/Whirlpool-Stats-Tool
-            fi
-
-            if [ -d "${HOME}"/boltzmann ]; then
-                cd "${HOME}"/boltzmann || exit
-
-                _print_message "Uninstalling Bolzmann..."
-
-                pipenv --rm &>/dev/null
-                cd - 1>/dev/null || exit
-                rm -rf "${HOME}"/boltzmann
-            fi
-
-            "${is_active_dojo_conf_backup}" && _backup_dojo_confs
-
-            rm -rf "${dojo_path}"
-
-            cd "${HOME}" || exit
-
-            sudo systemctl restart --quiet docker
-
-            _print_message "All RoninDojo features has been Uninstalled..."
-            _sleep
-        fi
-
-
-        ;;
-    7)
         bash -c "${ronin_system_menu}"
         exit
         ;;
